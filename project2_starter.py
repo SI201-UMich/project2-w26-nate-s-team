264b8e51871f014408ecf06ea2e6f31dd0aedf06
Nate Mitelman <mitelman@umich.edu>
2026-03-23 16:12:33 -0400

    test cases for test_load_listing_results

--- project2_starter.py
+++ project2_starter.py
@@ -203,9 +203,8 @@ class TestCases(unittest.TestCase):
         self.detailed_data = create_listing_database(self.search_results_path)
 
     def test_load_listing_results(self):
-        # TODO: Check that the number of listings extracted is 18.
-        # TODO: Check that the FIRST (title, id) tuple is  ("Loft in Mission District", "1944564").
-        pass
+        self.assertEqual(len(self.listings), 18)
+        self.assertEqual(self.listings[0], ("Loft in Mission District", "1944564"))
 
     def test_get_listing_details(self):
         html_list = ["467507", "1550913", "1944564", "4614763", "6092596"]
